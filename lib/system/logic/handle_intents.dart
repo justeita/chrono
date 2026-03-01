88ec9d9d2563f0b81e4e03585b55831aed839dba
justeita <94752137+justeita@users.noreply.github.com>
2026-03-01 08:28:50 +0700

    feat: handle android intents for alarms and timers

--- lib/system/logic/handle_intents.dart
+++ lib/system/logic/handle_intents.dart
@@ -8,11 +8,14 @@ import 'package:clock_app/developer/logic/logger.dart';
 import 'package:clock_app/navigation/types/app_visibility.dart';
 import 'package:clock_app/notifications/logic/alarm_notifications.dart';
 import 'package:clock_app/settings/types/listener_manager.dart';
+import 'package:clock_app/timer/types/time_duration.dart';
+import 'package:clock_app/timer/types/timer.dart';
+
 import 'package:flutter/material.dart' hide Intent;
 import 'package:receive_intent/receive_intent.dart';
 
 void handleIntent(Intent? receivedIntent, BuildContext context,
-    Function(Alarm) onSetAlarm, Function(int) setTab) async {
+    Function(Alarm) onSetAlarm, Function(int, [String?]) setTab) async {
   if (receivedIntent != null) {
     logger.i(
         "Intent received ${receivedIntent.action} ${receivedIntent.data} ${receivedIntent.extra}");
@@ -39,13 +42,10 @@ void handleIntent(Intent? receivedIntent, BuildContext context,
             receivedIntent.extra?["android.intent.extra.alarm.VIBRATE"];
         String? message =
             receivedIntent.extra?["android.intent.extra.alarm.MESSAGE"];
-        // string ringtone = receivedIntent.extra?["android.intent.extra.alarm.RINGTONE"];
         List<int>? days =
             receivedIntent.extra?["android.intent.extra.alarm.DAYS"];
         if (hour == null || minute == null || !skipUi) {
-          // print("Navigate to alarm screen");
           setTab(0);
-          // navigate to alarm screen and open ui
         } else {
           Alarm alarm =
               Alarm.fromTimeOfDay(TimeOfDay(hour: hour, minute: minute));
@@ -53,15 +53,11 @@ void handleIntent(Intent? receivedIntent, BuildContext context,
             alarm.setSetting(context, "Vibrate", vibration);
           }
           if (days != null) {
-            // In our system, Monday is 1, Sunday is 7
-            // In Android, Sunday is 1, Saturday is 7
-            // Rotate the days to match our system
             for (int i = 0; i < days.length; i++) {
               days[i] = (days[i] + 1) % 7;
               if (days[i] == 0) days[i] = 7;
             }
 
-            // The setting accepts a list of bools where Monday is index 0
             List<bool> settingDays = List.filled(7, false);
             for (int day in days) {
               settingDays[day - 1] = true;
@@ -79,18 +75,52 @@ void handleIntent(Intent? receivedIntent, BuildContext context,
           await saveList("alarms", alarms);
           onSetAlarm(alarm);
 
-          // Update the frontend UI if app is open
           ListenerManager.notifyListeners("alarms");
-          // setState(() {});
         }
         break;
+      case "android.intent.action.SNOOZE_ALARM":
+        setTab(0);
+        break;
       case "android.intent.action.SET_TIMER":
+        int? length =
+            receivedIntent.extra?["android.intent.extra.alarm.LENGTH"];
+        bool skipUiTimer =
+            receivedIntent.extra?["android.intent.extra.alarm.SKIP_UI"] ??
+                false;
+        String? messageTimer =
+            receivedIntent.extra?["android.intent.extra.alarm.MESSAGE"];
+
+        if (length == null || !skipUiTimer) {
+          setTab(2);
+        } else {
+          ClockTimer timer = ClockTimer(TimeDuration.fromSeconds(length));
+          if (messageTimer != null) {
+            timer.setSettingWithoutNotify("Label", messageTimer);
+          }
+          await timer.start();
+          List<ClockTimer> timers = await loadList<ClockTimer>("timers");
+          timers.add(timer);
+          await saveList("timers", timers);
+          ListenerManager.notifyListeners("timers");
+          // update timer notification using appropriate logic later
+        }
         break;
       case "android.intent.action.SET_STOPWATCH":
+        setTab(3); // Just open tab. Standard Google Assistant lacks this anyway
         break;
       case "android.intent.action.VIEW_ALARMS":
+      case "android.intent.action.SHOW_ALARMS":
+        setTab(0);
         break;
       case "android.intent.action.VIEW_TIMERS":
+      case "android.intent.action.SHOW_TIMERS":
+        setTab(2);
+        break;
+      case "android.intent.action.DISMISS_ALARM":
+        setTab(0);
+        break;
+      case "android.intent.action.DISMISS_TIMER":
+        setTab(2);
         break;
       case "SELECT_NOTIFICATION":
         appVisibilityWhenAlarmNotificationCreated = AppVisibility.state;
